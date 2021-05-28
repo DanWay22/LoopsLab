298314cae1eb2c280a4ac39fba473459d63c3175
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:16:13 -1000

    Add files via upload

---
+++ LoopFour/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace LoopsFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 1;
+
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= 4; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine(j);
+            }
+            Console.ReadLine();
+        }
+    }
+}
