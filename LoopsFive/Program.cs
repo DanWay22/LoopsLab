6f70ca947ceef9aa672136cb3dca0bc16e22aadf
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:16:47 -1000

    Add files via upload

---
+++ LoopsFive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace LoopsFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            
+            for (i = 1; i <= 3; i++)
+            {
+                    for (j = 1; j <= 4; j++)
+                    {
+                    Console.Write(i);
+                    }
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
