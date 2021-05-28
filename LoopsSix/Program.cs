d9ebe5cb3647f35dbc6e345e9113e5050112ee3f
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:17:30 -1000

    Add files via upload

---
+++ LoopsSix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace LoopsSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 4;
+            int i, j, k;
+            for (i = 1; i <= val; i++)
+            {
+                for (j = 1; j <= val - i; j++)
+                {
+                    // Console.Write("");  
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
