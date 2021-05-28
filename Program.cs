5b851a41f3c66ec767e9abd84abce22edb5c4abd
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:10:40 -1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace LoopsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int i = 0;
+         int nLines = 2;
+
+         StringBuilder sb = new StringBuilder();
+         sb.Append('*', 5);
+
+         while (i <= nLines)
+         {
+            Console.WriteLine(sb);
+            i++;
+          }
+         Console.Read();
+        }
+    }
+ }
+
