a85ca3b370c9955a16bfe0376b623a4569a591c4
Daniel Way <daniel.lee.way@gmail.com>
2021-05-27 16:13:08 -1000

    Add files via upload

---
+++ LoopsThree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace LoopsThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int nLines = 4;
+
+            while (i <= nLines)
+            {
+                StringBuilder sb = new StringBuilder();
+                if (i == 1)
+                {
+                    sb.Append('*', 4);
+                }
+                if (i == 2)
+                {
+                    sb.Append('*', 3);
+                }
+                if (i == 3)
+                {
+                    sb.Append('*', 2);
+                }
+                if (i == 4)
+                {
+                    sb.Append('*', 1);
+                }
+                Console.WriteLine(sb);
+                i++;
+            }
+            Console.Read();
+        }
+    }
+}
+
